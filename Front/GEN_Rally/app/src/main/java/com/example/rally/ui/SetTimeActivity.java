6776873a8d5301952ed33ed728ee5b6b87d2be3c
hakyuwon <kyu10225@naver.com>
2025-05-26 20:18:46 +0900

    refactor: 기능에 따라 이름 수정

--- Front/GEN_Rally/app/src/main/java/com/example/rally/ui/SetTimeActivity.java
+++ Front/GEN_Rally/app/src/main/java/com/example/rally/ui/SetTimeActivity.java
@@ -2,7 +2,6 @@ package com.example.rally.ui;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -25,7 +24,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class MatchActivity extends AppCompatActivity {
+public class SetTimeActivity extends AppCompatActivity {
         private RecyclerView rvDates, rvHours;
         private Button nextBtn;
         private HourAdapter hourAdapter;
@@ -53,7 +52,7 @@ public class MatchActivity extends AppCompatActivity {
             setupDateRecycler();
 
             nextBtn.setOnClickListener(v -> {
-                Intent intent = new Intent(this, MatApSingleActivity.class);
+                Intent intent = new Intent(this, SetLocationActivity.class);
                 intent.putExtra("date", selectedDate.toString());
                 intent.putIntegerArrayListExtra(
                         "hours", new ArrayList<>(selectedHours));
@@ -88,7 +87,7 @@ public class MatchActivity extends AppCompatActivity {
         boolean isEnabled = selectedDate != null && !selectedHours.isEmpty();
         nextBtn.setEnabled(isEnabled);
         nextBtn.setBackgroundResource(isEnabled ? R.drawable.bg_next_button_active : R.drawable.bg_next_button_inactive);
-        nextBtn.setTextColor(Color.parseColor(isEnabled ? "#FFFFFF" : "#676767"));
+        nextBtn.setTextColor(Color.parseColor(isEnabled ? "#FFFFFF" : "#AAAAAA"));
     }
 
         @SuppressLint("ClickableViewAccessibility")
@@ -123,7 +122,6 @@ public class MatchActivity extends AppCompatActivity {
                     if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL) {
                         v.performClick();
                     }
-
                     return false; // 스크롤은 유지
                 }
             });
