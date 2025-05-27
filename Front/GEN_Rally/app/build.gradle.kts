4caf20f6feeb41d78209e88990fbee7da45d96d8
hakyuwon <kyu10225@naver.com>
2025-05-27 23:26:41 +0900

    feat: 구글 맵 추가

--- Front/GEN_Rally/app/build.gradle.kts
+++ Front/GEN_Rally/app/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+
 }
