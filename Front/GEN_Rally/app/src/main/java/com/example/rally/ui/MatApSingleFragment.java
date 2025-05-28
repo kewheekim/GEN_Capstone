ae2ef8186d524a6005cdd8d14bd4b21611b94914
hakyuwon <kyu10225@naver.com>
2025-05-28 16:34:55 +0900

    style: 주석 추가

--- Front/GEN_Rally/app/src/main/java/com/example/rally/ui/MatApSingleFragment.java
+++ Front/GEN_Rally/app/src/main/java/com/example/rally/ui/MatApSingleFragment.java
@@ -33,7 +33,7 @@ public class MatApSingleFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_single1, container, false);
+        View view = inflater.inflate(R.layout.fragment_single, container, false);
 
         TextView questionText = view.findViewById(R.id.text_question);
         RadioGroup radioGroup = view.findViewById(R.id.radio_group);
