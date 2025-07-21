cffb5e6a162d0e392138e2858b44210caf1574ab
hakyuwon <kyu10225@naver.com>
2025-07-21 23:00:31 +0900

    feat: 네이버 로그인

--- Back/rally/src/main/java/com/gen/rally/exception/ErrorCode.java
+++ Back/rally/src/main/java/com/gen/rally/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     PASSWORD_MISMATCH("PASSWORD_MISMATCH", "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND("USER_NOT_FOUND", "해당 유저를 찾을 수 없습니다."),
     INVALID_TOKEN("INVALID_TOKEN", "유효하지 않은 토큰입니다."),
+    INVALID_STATE("INVALID_STATE","유효하지 않은 요청입니다."),
     INVALID_KAKAO_CODE("INVALID_KAKAO_CODE", "카카오 인가 코드가 유효하지 않습니다."),
+    INVALID_NAVER_CODE("INVALID_NAVER_CODE","네이버 인가 코드가 유효하지 않습니다."),
     UNAUTHORIZED("UNAUTHORIZED", "인증이 필요합니다."),
     FORBIDDEN("FORBIDDEN", "접근 권한이 없습니다."),
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다.");
--- Back/rally/src/main/java/com/gen/rally/exception/ErrorResponse.java
+++ Back/rally/src/main/java/com/gen/rally/exception/ErrorResponse.java
@@ -3,11 +3,14 @@ package com.gen.rally.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class ErrorResponse {
     private String message;
     private String code;
+    private final LocalDateTime timestamp = LocalDateTime.now();
 
     public static ErrorResponse from(ErrorCode errorCode) {
         return new ErrorResponse(errorCode.getMessage(), errorCode.getCode());
