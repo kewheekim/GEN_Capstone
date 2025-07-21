299019da915649b6333b3dcd4c98da84d310d5ff
hakyuwon <kyu10225@naver.com>
2025-07-21 23:01:04 +0900

    fix: dto 수정

--- Back/rally/src/main/java/com/gen/rally/dto/auth/SocialSignupRequest.java
+++ Back/rally/src/main/java/com/gen/rally/dto/auth/SocialSignupRequest.java
@@ -1,13 +1,13 @@
-package com.gen.rally.dto;
+package com.gen.rally.dto.auth;
 
 import com.gen.rally.enums.Gender;
+import com.gen.rally.enums.LoginType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SocialSignupRequest {
-    private String socialId;
     private String name;
     private byte[] profileImage;
     private Gender gender;
