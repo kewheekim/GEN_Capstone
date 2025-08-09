0500c5c674c87230426e57d13afbfc8ae3aac7df
hakyuwon <kyu10225@naver.com>
2025-08-09 23:32:00 +0900

    fix: getter

--- Front/GEN_Rally/app/src/main/java/com/example/rally/dto/CheckIdRequest.java
+++ Front/GEN_Rally/app/src/main/java/com/example/rally/dto/CheckIdRequest.java
@@ -1,5 +1,8 @@
 package com.example.rally.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CheckIdRequest {
     private String userId;
     public CheckIdRequest(String userId) { this.userId = userId; }
