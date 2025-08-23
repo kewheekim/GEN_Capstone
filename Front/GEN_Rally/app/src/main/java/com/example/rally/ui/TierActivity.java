9592c9a23030e03b9df38273807f6f014cd395f0
hakyuwon <kyu10225@naver.com>
2025-08-24 02:51:30 +0900

    feat: 실력진단 완료 액티비티

--- Front/GEN_Rally/app/src/main/java/com/example/rally/ui/TierActivity.java
+++ Front/GEN_Rally/app/src/main/java/com/example/rally/ui/TierActivity.java
@@ -1,16 +1,26 @@
 package com.example.rally.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.rally.R;
+import com.example.rally.api.ApiService;
+import com.example.rally.api.RetrofitClient;
+import com.example.rally.dto.TierAssessRequest;
+import com.example.rally.dto.TierAssessResponse;
 
 import javax.annotation.Nullable;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 // SIG_012
 public class TierActivity extends AppCompatActivity {
@@ -29,6 +39,53 @@ public class TierActivity extends AppCompatActivity {
         ivTier = findViewById(R.id.iv_tier);
         btnNext = findViewById(R.id.btn_next);
 
+        int canService = getIntent().getIntExtra("canService", -1);
+        int canPass    = getIntent().getIntExtra("canPass", -1);
+        int footWork   = getIntent().getIntExtra("footWork", -1);
+        int count      = getIntent().getIntExtra("count", -1);
+        int howLong    = getIntent().getIntExtra("howLong", -1);
 
+        final ApiService apiService = RetrofitClient
+                .getSecureClient(getApplicationContext(),"http://10.0.2.2:8080/")
+                .create(ApiService.class);
+
+        TierAssessRequest req = new TierAssessRequest();
+        req.setQ1(canService);
+        req.setQ2(canPass);
+        req.setQ3(footWork);
+        req.setQ4(count);
+        req.setQ5(howLong);
+
+        apiService.getTier(req)
+                .enqueue(new Callback<TierAssessResponse>() {
+                    @Override
+                    public void onResponse(Call<TierAssessResponse> call, Response<TierAssessResponse> response) {
+                        Log.d("TierActivity", "onResponse ✅ code=" + response.code());
+                        if(response.isSuccessful() && response.body()!=null){
+                            TierAssessResponse body = response.body();
+                            tvTier.setText(body.getTier());
+                            tvName.setText(body.getNickname()+"님은");
+                        } else {
+                            // 401이면 토큰 만료/부재 가능
+                            if (response.code() == 401) {
+                                Log.e("getTier","오류:"+ response.body());
+                                Toast.makeText(TierActivity.this, "인증이 만료되었습니다", Toast.LENGTH_LONG).show();
+                                // TODO: 토큰 삭제 및 로그인 화면 이동
+                                // new TokenStore(getApplicationContext()).clear();
+                                // startActivity(new Intent(TierActivity.this, AuthActivity.class));
+                                finish();
+                            } else {
+                                Log.e("getTier","오류:"+ response.body());
+                                Toast.makeText(TierActivity.this, "오류: " + response.code(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<TierAssessResponse> call, Throwable t) {
+                        Log.e("getTier","onFailure " ,t);
+                        Toast.makeText(TierActivity.this, "네트워크 오류: " + t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
