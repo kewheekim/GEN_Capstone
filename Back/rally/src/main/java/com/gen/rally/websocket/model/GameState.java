4a6e908ba8896f93f14f9040ec45915926ca6349
coals <studycoals329@gmail.com>
2025-08-27 13:39:57 +0900

    fix: GameState 경기 시간 필드 추가

--- Back/rally/src/main/java/com/gen/rally/websocket/model/GameState.java
+++ Back/rally/src/main/java/com/gen/rally/websocket/model/GameState.java
@@ -11,6 +11,12 @@ public class GameState {
     public int user1Score = 0, user2Score = 0;
     public int user1Sets = 0, user2Sets = 0;
     public String currentServe = "USER1";
+    // 경기 시간
+    public long startAt = 0L;
+    public boolean paused = true;
+    public long pauseStartedAt = 0L;
+    public long totalPaused = 0L;   // 누적 일시정지 시간(ms)
+
     public Set<String> appliedMsgIds = ConcurrentHashMap.newKeySet();
     public Deque<String> recentEvents = new ArrayDeque<>();
 }
