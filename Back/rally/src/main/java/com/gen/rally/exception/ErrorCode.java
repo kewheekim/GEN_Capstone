b29b8a4a6dde2163bcf029311942f572c6e37f49
hakyuwon <kyu10225@naver.com>
2025-09-19 01:57:32 +0900

    feat: 메시지, 경기 오류

--- Back/rally/src/main/java/com/gen/rally/exception/ErrorCode.java
+++ Back/rally/src/main/java/com/gen/rally/exception/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     INVALID_STATE("INVALID_STATE", "유효하지 않은 요청입니다.", HttpStatus.BAD_REQUEST),
     INVALID_KAKAO_CODE("INVALID_KAKAO_CODE", "카카오 인가 코드가 유효하지 않습니다.", HttpStatus.BAD_REQUEST),
     INVALID_NAVER_CODE("INVALID_NAVER_CODE", "네이버 인가 코드가 유효하지 않습니다.", HttpStatus.BAD_REQUEST),
+    INVALID_MESSAGE("INVALID_MESSAGE","유효하지 않은 메시지입니다.", HttpStatus.BAD_REQUEST),
 
     // 401 Unauthorized
     INVALID_TOKEN("INVALID_TOKEN", "유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
@@ -24,6 +25,7 @@ public enum ErrorCode {
 
     // 404 Not Found
     USER_NOT_FOUND("USER_NOT_FOUND", "해당 유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    GAME_NOT_FOUND("GAME_NOT_FOUND","해당 게임을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 
     // 500 Internal Server Error
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
