1207cd3d18ce826695a8c94674da4e8eed979cfd
hakyuwon <kyu10225@naver.com>
2025-10-06 16:23:49 +0900

    fix: 홈 화면 연결

--- Front/GEN_Rally/app/src/main/java/com/example/rally/ui/TutorialActivity.java
+++ Front/GEN_Rally/app/src/main/java/com/example/rally/ui/TutorialActivity.java
@@ -1,5 +1,6 @@
 package com.example.rally.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -50,15 +51,17 @@ public class TutorialActivity extends AppCompatActivity {
         btnNext.setEnabled(true);
         btnNext.setTextColor(getColor(R.color.white));
         btnNext.setOnClickListener(v -> {
-            if (page < PAGES.length){
+            if (page < PAGES.length - 1) {
                 page++;
                 showPage(page);
                 updateProgress();
-            }else{
+            }
+            else if (page == PAGES.length - 1) {
+                Intent intent = new Intent(TutorialActivity.this, MainActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
-
     }
 
     private void showPage(int idx) {
