b8ecafbc41a7ce89621e71cbf62bbf4bb04a12c6
coals <studycoals329@gmail.com>
2025-10-15 02:26:34 +0900

    fix: 경기 종료 gif 1회 재생으로 수정

--- Front/GEN_Rally/app/src/main/java/com/example/rally/ui/GameFinishActivity.java
+++ Front/GEN_Rally/app/src/main/java/com/example/rally/ui/GameFinishActivity.java
@@ -1,24 +1,52 @@
 package com.example.rally.ui;
 
+import com.example.rally.R;
 import android.os.Bundle;
 import android.widget.ImageView;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.bumptech.glide.Glide;
-import com.example.rally.R;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
+// MAT_END_001
 public class GameFinishActivity extends AppCompatActivity {
-
     ImageView ivFinishGif;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_finish);
 
-        // gif 불러오기
         ivFinishGif = findViewById(R.id.iv_finish_gif);
+
         Glide.with(this).asGif().load(R.drawable.game_finish_gif)
+                .listener(new RequestListener<GifDrawable>() {
+                    @Override
+                    public boolean onLoadFailed(
+                            @Nullable GlideException e,
+                            Object model,
+                            Target<GifDrawable> target,
+                            boolean isFirstResource
+                    ) {
+                        return false;
+                    }
+                    @Override
+                    public boolean onResourceReady(
+                            @NonNull GifDrawable resource,
+                            Object model,
+                            Target<GifDrawable> target,
+                            DataSource dataSource,
+                            boolean isFirstResource
+                    ) {
+                        resource.setLoopCount(1);
+                        return false;
+                    }
+                })
                 .into(ivFinishGif);
     }
 }
