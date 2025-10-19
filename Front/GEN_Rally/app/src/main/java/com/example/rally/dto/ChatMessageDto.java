1ffb067018e0024553d572b0387f52614c28fe1f
hakyuwon <kyu10225@naver.com>
2025-10-20 01:54:03 +0900

    feat: message type - text, match_card

--- Front/GEN_Rally/app/src/main/java/com/example/rally/dto/ChatMessageDto.java
+++ Front/GEN_Rally/app/src/main/java/com/example/rally/dto/ChatMessageDto.java
@@ -9,13 +9,15 @@ public class ChatMessageDto {
     private Long id;
     private Long roomId;
     private Long senderId;
+    private String type;
     private String content;
     private String createdAt;
 
-    public ChatMessageDto (Long id, Long roomId, Long senderId, String content, String createdAt){
+    public ChatMessageDto (Long id, Long roomId, Long senderId, String type, String content, String createdAt){
         this.id = id;
         this.roomId = roomId;
         this.senderId = senderId;
+        this.type = type;
         this.content = content;
         this.createdAt = createdAt;
     }
